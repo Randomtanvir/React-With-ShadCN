ba1b1de8617b3b087a87808e9a34d53588067a13
Tanvir <tanvir.info07@gmail.com>
2024-09-19 12:29:09 +0600

    Project setup react with Shad-cn

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen  flex justify-center items-center">
+        React fresh with shad-cn
+      </div>
+    </>
+  );
+}
+
+export default App;
